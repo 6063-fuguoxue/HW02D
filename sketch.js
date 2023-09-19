e34d608fd63e4d3d820c098821e3f1a29606c5f6
Fuguo Xue <fx2100@nyu.edu>
2023-09-19 12:52:44 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,34 @@
+let width = 400;
+let height = 400;
+
 function setup() {
+    createCanvas(windowWidth, windowHeight); // w, h
 }
 
 function draw() {
+    stroke(0);
+    // draw a rectangle to define the area
+    line(0,0,width,0);
+    line(0,0,0,height);
+    line(width,0,width,height);
+    line(0,height,width,height);
+
+    // draw two triangles
+    fill(0);
+    triangle(0, 0, 100, 0, 0, 200);
+    triangle(400,400,300,400,400,200);
+    beginShape();
+    vertex(100, 400);
+    vertex(0, 400);
+    vertex(0, 200);
+    vertex(100, 200);
+    vertex(100,100);
+    vertex(150,100);
+    vertex(250,300);
+    vertex(300,300);
+    vertex(300,200);
+    vertex(400,200);
+    vertex(400,0);
+    vertex(300,0);
+    endShape(CLOSE);
 }
